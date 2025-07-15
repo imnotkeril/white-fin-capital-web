159dac300575ddc49928ae56f31e1fb5a9ff1e8f
Keril 🔮 <118714855+imnotkeril@users.noreply.github.com>
2025-07-16 06:36:38 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     optimizeCss: true,
   },
   images: {
+    unoptimized: true,
     domains: ['localhost'],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
